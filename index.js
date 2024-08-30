58d9c8532e54c388c4bcf9aede1539b37136e07d
Aminur-Rahman-Kanon <kanon754@gmail.com>
2024-08-30 02:23:48 +0100

    testing short url api

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ require('dotenv').config();
 
 app.get('/:item', async (req, res) => {
     const { item } = req.params;
+    if (!item) return res.status(400).json({ message:'invalid request' });
     await fetch('https://boxdelabonita.com/api/fetch-all-products')
     .then(jsn => jsn.json())
-    .then(result => console.log(result, item))
-    .catch(err => console.log(err))
+    .then(result => {
+        if (!result.data) return res.status(500).json({ message: 'server error' });
+        const filteredItem = result.data.filter(itm => itm.title === item)[0];
+        if (filteredItem){
+            return res.redirect(`https://boxdelabonita.com/bag/${filteredItem.category}/${item}`);
+        }
+    })
+    .catch(err => res.status(500).json({ message: err }));
 })
 
 const port = process.env.PORT || '5000'
